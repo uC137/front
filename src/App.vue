b4e1dd1568867281c790d2fa367cedbc9edd157e
UchMan <ucha@clips4sale.com>
2019-01-07 03:00:49 +0400

    basic app setup

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <AppNavbar></AppNavbar>
+        <router-view></router-view>
+        <AppFooter></AppFooter>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import AppNavbar from '@/components/AppNavbar.vue';
+import AppFooter from '@/components/AppFooter.vue';
+
+@Component({
+    components: {
+        AppNavbar, AppFooter
+    }
+})
+export default class App extends Vue {
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
